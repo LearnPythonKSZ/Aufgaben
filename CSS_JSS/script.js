ae531be32e5e2dbefc5fde3370f9219a270344f2
LearnPythonKSZ <33370042+LearnPythonKSZ@users.noreply.github.com>
2018-01-21 20:07:51 +0100

    Update script.js

--- CSS_JSS/script.js
+++ CSS_JSS/script.js
@@ -1,27 +1,45 @@
-function Question(name){
-	var elements = document.getElementsByName(name);
-	var bool = elements[0].getAttribute( 'id' );
-	if (bool=="true"){
-		document.getElementById("answer1").className="answer";
-		document.getElementById("answer2").className="answer";
-		document.getElementById("answer3").className="answer";
-		document.getElementById("answer4").className="answer";
-		document.getElementById("next").className="nextunlocked";
-		document.getElementById("next").value="Nächstes Kapitel";
-	}else{
-		switch(name){
-			case "button1":
-				document.getElementById("answer1").className="answercrossed";
-				break;
-			case "button2":
-				document.getElementById("answer2").className="answercrossed";
-				break;
-			case "button3":
-				document.getElementById("answer3").className="answercrossed";
-				break;
-			case "button4":
-				document.getElementById("answer4").className="answercrossed";
-				break;
-		}
+function solution(idbutton){
+ 	if (idbutton.getAttribute("id")=="solutionbutton1"){
+ 		var solutiontag=document.getElementById("solutiontag1");
+ 		var solutionbutton=document.getElementById("solutionbutton1");
+ 		var solutionspan=document.getElementById("solution1");
+ 		solutionbutton.style.width = "800px";
+ 		solutionbutton.style.height = "100px";
+ 		solutionbutton.style.left="calc(50% - 400px)";
+ 		solutiontag.style.display="none";
+ 		solutionspan.style.display="inline";
+ 	}else if(idbutton.getAttribute("id")=="solutionbutton2"){
+ 		var solutiontag=document.getElementById("solutiontag2");
+ 		var solutionbutton=document.getElementById("solutionbutton2");
+ 		var solutionspan=document.getElementById("solution2");
+ 		solutionbutton.style.width = "800px";
+ 		solutionbutton.style.height = "100px";
+ 		solutionbutton.style.left="calc(50% - 400px)";
+ 		solutiontag.style.display="none";
+ 		solutionspan.style.display="inline";
+ 	}else if(idbutton.getAttribute("id")=="solutionbutton3"){
+		var solutiontag=document.getElementById("solutiontag3");
+ 		var solutionbutton=document.getElementById("solutionbutton3");
+ 		var solutionspan=document.getElementById("solution3");
+ 		solutionbutton.style.width = "800px";
+ 		solutionbutton.style.height = "100px";
+ 		solutionbutton.style.left="calc(50% - 400px)";
+ 		solutiontag.style.display="none";
+ 		solutionspan.style.display="inline";
+	}else if(idbutton.getAttribute("id")=="solutionbutton4"){
+		var solutiontag=document.getElementById("solutiontag4");
+ 		var solutionbutton=document.getElementById("solutionbutton4");
+ 		var solutionspan=document.getElementById("solution4");
+ 		solutionbutton.style.width = "800px";
+ 		solutionbutton.style.height = "100px";
+ 		solutionbutton.style.left="calc(50% - 400px)";
+ 		solutiontag.style.display="none";
+ 		solutionspan.style.display="inline";
 	}
-}
+ }
+ function help(idtext,idbutton){
+	var idh = idbutton.getAttribute("id");
+ 	idbutton.style.display = "none" ;
+ 	idtext.style.display = "block";
+     alert("an error occured pls contact system admin #1");
+ }
